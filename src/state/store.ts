45a9b998457f5f09ef057f3a8cd31d3b4861943a
Gustavo Pérez Machado <gustperz@gmail.com>
2021-07-31 11:28:26 -0500

    own config
    configure eslint whit prettier
    install tailwindcss
    and rerfactor folder basic structure

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,7 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
+import counterReducer from 'features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
@@ -15,3 +17,7 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   unknown,
   Action<string>
 >;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
